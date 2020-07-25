f06064d4d842be5e9cf9880c2389874973712fa5
Vicheka <phorvicheka@yahoo.com>
2020-07-25 12:37:11 +0700

    RMNE 13 - Theme Variables
    In order to:
     * app should have well defined font sizes, colors, spacing, etc
     * app looks consistent across screens
     * maintaining a convention across the app
     * avoid making changes in the entire app and replace every instance of one style: for example the fontSize
    
    We can:
    1. Create style \app\styles\theme.style.js
    2. apply theme style in style js file and use it in the react component:
                    app\components\Home\Home.component.style.js
                    app\components\Home\Home.component.js
                    app\components\TextArea\TextArea.component.style.js
    3. If error on commit because of pre-commit hook of huskyrc.json:
      * run `npm test -- -u` to update snapshot: __tests__\__snapshots__\Home.component.test.js.snap
    
    Two benefits:
     * If our business team now tell us to change the font sizes, we can change the theme variables at one place and it gets reflected in the entire app.
     * This will enable us to write multiple theme files which in turn adds basic theming support to our app. For example we can write two theme files - one for a light theme and one for a dark theme and give our app users the option to switch between the themes.

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -2,15 +2,13 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import styles from './Home.component.style';
 import TextArea from '../TextArea/TextArea.component';
-import Counter from '../Counter/Counter.component';
 
 class Home extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text> Please enter your note here</Text>
-        <TextArea />
-        <Counter initialCountValue={5} />
+        <Text style={styles.textAreaTitle}> Please enter your note here</Text>
+        <TextArea style={styles.textArea}/>
       </View>
     );
   }
